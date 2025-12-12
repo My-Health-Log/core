c89871d6c5e6c25b397fbc22fd78d78c2eda38b2
Ayush Pahwa <ayush.pahwa@automattic.com>
2025-12-12 08:33:05 +0530

    update: add env setup

--- src/server/src/config.ts
+++ src/server/src/config.ts
@@ -3,7 +3,7 @@ import z from "zod";
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'staging', 'test']).default('development'),
   PORT: z.string().default('3000').transform(Number),
-  HOST: z.string().default('localhost'),
+  HOST: z.string().default('http://localhost'),
   // TODO: update this to min length 1 when db is added
   DB_URL: z.string().default(''),
 });
