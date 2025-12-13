011b33adda51ad5aa50a76897267e17fff67930c
Ayush Pahwa <ayush.pahwa@automattic.com>
2025-12-13 18:27:16 +0530

    create: add common response schemas

---
+++ src/server/src/schemas/common.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const responseSchema = (dataSchema = {}) => ({
+  200: z.object({
+    success: z.boolean(),
+    message: z.string(),
+    data: z.object(dataSchema).optional(),
+  }),
+  400: z.object({
+    success: z.boolean(),
+    message: z.string(),
+    error: z.object({ message: z.string() }).optional(),
+  }),
+});
