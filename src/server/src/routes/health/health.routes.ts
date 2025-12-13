a3277d9c11650e16ac1a7ef61a1ff2e521889a1b
Ayush Pahwa <ayush.pahwa@automattic.com>
2025-12-13 16:59:39 +0530

    update: added endpoints for ai health check

--- src/server/src/routes/health/health.routes.ts
+++ src/server/src/routes/health/health.routes.ts
@@ -1,16 +1,25 @@
 import { FastifyInstance } from "fastify";
 import { z } from 'zod'
+import { askAi } from "../../services/ai.js";
 
-const pingSchema = {
+const healthSchema = {
   tags: ['health'],
   summary: 'Health check',
   response: {
     200: z.object({ pong: z.string() })
   }
 }
+const aiHealthSchema= {
+  ...healthSchema,
+  summary:"Check health of the AI sdk"
+}
 
 export default async function healthRoutes(app: FastifyInstance) {
-  app.get('/ping', { schema: pingSchema }, async () => {
+  app.get('/ping', { schema: healthSchema }, async () => {
     return { pong: 'it worked!' }
   });
+
+  app.get('/ai', {schema:aiHealthSchema}, async()=>{
+    return await askAi();
+  })
 }
