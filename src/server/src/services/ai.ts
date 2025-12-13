3c70e278fd811c7109732afc4da7a1cecee0c93a
Ayush Pahwa <ayush.pahwa@automattic.com>
2025-12-13 17:06:15 +0530

    update: fix zod schema error

--- src/server/src/services/ai.ts
+++ src/server/src/services/ai.ts
@@ -3,16 +3,14 @@ import { GoogleGenerativeAIModelId } from "@ai-sdk/google/internal";
 import { generateObject } from "ai";
 import z from "zod";
 
-const HEALTH_CHECK_PROMPT = `
-You are a helpful assistant to help with health test of an API.
-`;
+const HEALTH_CHECK_PROMPT = 'You are a helpful assistant to help with health test of an API.';
 const DEFAULT_MODEL_ID:GoogleGenerativeAIModelId = 'gemini-2.5-flash-lite';
 
 const HEALTH_CHECK_SCHEMA =z.object({
     success: z.boolean(),
     message: z.string(),
-    data: z.object().optional(),
-    error: z.object().optional(),
+    data: z.object({}).optional(),
+    error: z.object({}).optional(),
   });
 
 export async function askAi(prompt=HEALTH_CHECK_PROMPT,modelName = DEFAULT_MODEL_ID, schema=HEALTH_CHECK_SCHEMA){
