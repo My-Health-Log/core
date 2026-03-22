3a90db8c75e03c066262a3376e4ed0159dc06263
Ayush Pahwa <ayush.pahwa@automattic.com>
2026-03-22 12:42:59 +0700

    update: import document stream from the correct source

--- src/pdf-parse/app/services/docling.py
+++ src/pdf-parse/app/services/docling.py
@@ -1,8 +1,9 @@
-import io
+from io import BytesIO
 
-from docling.datamodel.base_models import DocumentStream, InputFormat
+from docling.datamodel.base_models import InputFormat
 from docling.datamodel.pipeline_options import PdfPipelineOptions
 from docling.document_converter import DocumentConverter, PdfFormatOption
+from docling_core.types.io import DocumentStream
 from fastapi import HTTPException, UploadFile
 
 # from app.schemas.extract import ExtractionResponse
@@ -12,9 +13,11 @@ from app.services.provider import ExtractionProvider
 class DoclingExtractionProvider(ExtractionProvider):
     def __init__(self):
         pipeline_options = PdfPipelineOptions()
+        # assumes the the PDF is text based and not images
         pipeline_options.do_ocr = False
         pipeline_options.do_table_structure = True
-        pipeline_options.generate_picture_images = False  # disables image extraction
+        # disables image extraction
+        pipeline_options.generate_picture_images = False
 
         self.converter = DocumentConverter(
             allowed_formats=[InputFormat.PDF],
@@ -27,11 +30,10 @@ class DoclingExtractionProvider(ExtractionProvider):
     async def extract(self, file: UploadFile) -> dict:
         if file.size is None or file.filename is None:
             raise HTTPException(404, "Please use a valid file")
-        print(file.size)
         if file.size > 5 * 1024 * 1024:
             raise HTTPException(413, "Please use files under 5 MB")
         content = await file.read()
-        stream = DocumentStream(name=file.filename or "", stream=io.BytesIO(content))
+        stream = DocumentStream(name=file.filename or "", stream=BytesIO(content))
         result = self.converter.convert(stream)
         doc = result.document
         # TODO: fix the mapping to the response object
