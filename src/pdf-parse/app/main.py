537c96d525ec8fea4bdf25293fe7930e1a7e4852
Ayush Pahwa <ayush.pahwa@automattic.com>
2026-04-11 14:39:54 +0700

    update: allow cors methods

--- src/pdf-parse/app/main.py
+++ src/pdf-parse/app/main.py
@@ -38,6 +38,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.cors_origins,
     allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 # Register exception handlers
